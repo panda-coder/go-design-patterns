ae32a82e2dda5d2ee196319b3e0d37456f0d54c4
Ercy Moreira Neto <ercym.neto@gmail.com>
2023-03-15 21:58:32 -0300

    feat: add wrong single resposability

---
+++ README.md
@@ -0,0 +1,19 @@
+# Go Design Patterns
+Repositório com o intuito de realizar estudo em cima de design patterns applicado a GoLang.
+
+
+## Tópicos cobertos:
+
+### Solid
+  01. *Single Responsiblity Principle* [Wrong](./solid/wrong/01_single_responsability.go) | [Refactored](./solid/refactored/01_single_responsability.go) 
+  02. *Open-Closed Principle* [Wrong](./solid/wrong/02_open_closed.go) | [Refactored](./solid/refactored/02_open_closed.go) 
+  03. *Liskov Substitution Principle* [Wrong](./solid/wrong/03_liskov_substitution.go) | [Refactored](./solid/refactored/03_liskov_substitution.go) 
+  04. Interface Segregation Principle* [Wrong](./solid/wrong/04_interface_segregation.go) | [Refactored](./solid/refactored/04_interface_segregation.go) 
+  05. *Dependency Inversion Principle* [Wrong](./solid/wrong/05_dependency_inversion.go) | [Refactored](./solid/refactored/05_dependency_inversion.go) 
+### Functional
+  01. [Lazy Evaluation]()
+
+
+## License
+
+[MIT](./LICENSE)
---
+++ solid/wrong/01_single_responsability.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Product struct {
+	Code        int    `json:"code"`
+	Description string `json:"description"`
+}
+
+type Order struct {
+	Id       int        `json:"id"`
+	Products []*Product `json:"products"`
+}
+
+func (o *Order) AddProduct(code int, description string) {
+	o.Products = append(o.Products, &Product{
+		Code:        code,
+		Description: description,
+	})
+}
+
+func (o *Order) Count() int {
+	return len(o.Products)
+}
+
+// WRONG
+func (o *Order) MustSave() {
+	fileName := fmt.Sprintf(
+		"order_%d.txt", o.Id,
+	)
+	f, err := os.Create(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	content, err := json.Marshal(o)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = f.WriteString(string(content))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	order := Order{Id: 1}
+	order.AddProduct(1, "Pepperoni Pizza")
+	order.AddProduct(2, "Marguerita Pizza")
+	order.MustSave()
+}
