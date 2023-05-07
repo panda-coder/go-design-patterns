43b549dedda1451665654c8a848950a7a70495ae
Ercy Moreira Neto <ercym.neto@gmail.com>
2023-05-07 16:07:55 -0300

    feat: add srp refactored--

--- solid/refactored/01_single_responsability.go
+++ solid/refactored/01_single_responsability.go
@@ -24,38 +24,55 @@ func (o *Order) AddProduct(code int, description string) {
 	})
 }
 
+func (o *Order) ToJSON() ([]byte, error) {
+	return json.Marshal(o)
+}
+func (o *Order) GetId() int {
+	return o.Id
+}
+
 func (o *Order) Count() int {
 	return len(o.Products)
 }
 
-// WRONG
-func (o *Order) MustSave() {
-	fileName := fmt.Sprintf(
-		"order_%d.txt", o.Id,
-	)
-	f, err := os.Create(fileName)
+type DataModel interface {
+	GetId() int
+	ToJSON() ([]byte, error)
+}
 
+type DataSaver interface {
+	Save(d DataModel) error
+}
+
+type FileDataSaver struct{}
+
+func (fos FileDataSaver) Save(prefix string, d DataModel) error {
+	fileName := fmt.Sprintf("%s_%d.txt", prefix, d.GetId())
+	f, err := os.Create(fileName)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer f.Close()
 
-	content, err := json.Marshal(o)
+	content, err := d.ToJSON()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	_, err = f.WriteString(string(content))
-
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	return nil
 }
 
 func main() {
 	order := Order{Id: 1}
 	order.AddProduct(1, "Pepperoni Pizza")
 	order.AddProduct(2, "Marguerita Pizza")
-	order.MustSave()
-
+	fileSaver := FileDataSaver{}
+	err := fileSaver.Save("order", &order)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
